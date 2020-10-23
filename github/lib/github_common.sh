11182c3fb53892da8bce40b2a60da009c9c8736f
Chris Evich <cevich@redhat.com>
2020-10-23 09:40:37 -0400

    Add shortcut for loading all common libraries
    One internal, and several external users of this library don't bother
    sourcing individual `common/lib/*.sh` files.  Instead they iterate
    over all of them by name, which is messy and inconvenient.  Add a
    short-cut script which callers can source to load all common libraries.
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- github/lib/github_common.sh
+++ github/lib/github_common.sh
@@ -4,21 +4,20 @@
 
 # Important paths defined here
 AUTOMATION_LIB_PATH="${AUTOMATION_LIB_PATH:-$(realpath $(dirname ${BASH_SOURCE[0]})/../../common/lib)}"
-source $AUTOMATION_LIB_PATH/anchors.sh || exit 1
 
 # Override default library message prefixes to those consumed by Github Actions
 # https://help.github.com/en/actions/reference/workflow-commands-for-github-actions
 # Doesn't work properly w/o $ACTIONS_STEP_DEBUG=true
 DEBUG_MSG_PREFIX="::debug::"
 # Translation to usage throughout common-library
-if [[ "$ACTIONS_STEP_DEBUG" == 'true' ]]; then
+if [[ "${ACTIONS_STEP_DEBUG:-false}" == 'true' ]]; then
     DEBUG=1
 fi
 # Highlight these messages in the Github Action WebUI
 WARNING_MSG_PREFIX="::warning::"
 ERROR_MSG_PREFIX="::error::"
-source $AUTOMATION_LIB_PATH/defaults.sh || exit 1
-source $AUTOMATION_LIB_PATH/console_output.sh || exit 1
+
+source $AUTOMATION_LIB_PATH/common_lib.sh || exit 1
 
 # usage: set_out_var <name> [value...]
 set_out_var() {
