adda8b1c7678c725ad9341c9a6d873f5477a2c3c
Chris Evich <cevich@redhat.com>
2023-06-22 10:05:58 -0400

    Support $SUDO setup for debian environments
    This is esp. used during CI VM image builds where only a user account is
    available for some stages.  It's important to block `sudo apt-get` calls
    from asking for user input during update/install.
    
    Signed-off-by: Chris Evich <cevich@redhat.com>

--- common/lib/platform.sh
+++ common/lib/platform.sh
@@ -2,16 +2,17 @@
 # Library of os/platform related definitions and functions
 # Not intended to be executed directly
 
-OS_RELEASE_VER="$(source /etc/os-release; echo $VERSION_ID | tr -d '.')"
-OS_RELEASE_ID="$(source /etc/os-release; echo $ID)"
-OS_REL_VER="$OS_RELEASE_ID-$OS_RELEASE_VER"
+OS_RELEASE_VER="${OS_RELEASE_VER:-$(source /etc/os-release; echo $VERSION_ID | tr -d '.')}"
+OS_RELEASE_ID="${OS_RELEASE_ID:-$(source /etc/os-release; echo $ID)}"
+OS_REL_VER="${OS_REL_VER:-$OS_RELEASE_ID-$OS_RELEASE_VER}"
 
-SUDO=""
-if [[ "$UID" -ne 0 ]]; then
-    SUDO="sudo"
-fi
-
-if [[ "$OS_RELEASE_ID" == "ubuntu" ]]; then
-    export DEBIAN_FRONTEND=noninteractive
-    SUDO="$SUDO env DEBIAN_FRONTEND=$DEBIAN_FRONTEND"
+# Ensure no user-input prompts in an automation context
+export DEBIAN_FRONTEND="${DEBIAN_FRONTEND:-noninteractive}"
+if ((UID)) || ((_TEST_UID)); then
+    SUDO="${SUDO:-sudo}"
+    if [[ "$OS_RELEASE_ID" =~ (ubuntu)|(debian) ]]; then
+        if [[ ! "$SUDO" =~ noninteractive ]]; then
+            SUDO="$SUDO env DEBIAN_FRONTEND=$DEBIAN_FRONTEND"
+        fi
+    fi
 fi
